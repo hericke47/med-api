1a0eee0a5591c1765386160ab650c794774a8669
Herick Exterkoetter <herickherick47@gmail.com>
2023-06-29 20:59:42 -0300

    doc: add swagger on refresh doctor token

--- src/doc/paths/index.ts
+++ src/doc/paths/index.ts
@@ -1,6 +1,8 @@
-import { authenticateDoctor, createDoctor } from "./doctors.swagger";
+import { createDoctor } from "./doctors.swagger";
+import { authenticateDoctor, refreshDoctorToken } from "./sessions.swagger";
 
 export default {
   "/doctors": createDoctor,
   "/sessions": authenticateDoctor,
+  "/sessions/refreshToken": refreshDoctorToken,
 };
