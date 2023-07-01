b5e280e8c391fe28bd448875efdfcde8dc1b9ccd
Herick Exterkoetter <herickherick47@gmail.com>
2023-07-01 18:12:03 -0300

    feat: add celebrate validation on create patient

--- src/shared/infra/http/routes/patients.routes.ts
+++ src/shared/infra/http/routes/patients.routes.ts
@@ -1,5 +1,6 @@
 import { CreatePatientController } from "@modules/patients/useCases/CreatePatient/CreatePatientController";
 import { Router } from "express";
+import { Joi, Segments, celebrate } from "celebrate";
 import { ensureDoctorAuthenticated } from "../middlewares/ensureDoctorAuthenticated";
 
 const patientsRouter = Router();
@@ -8,6 +9,17 @@ const createPatientController = new CreatePatientController();
 
 patientsRouter.post(
   "/",
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      birthDate: Joi.date().required(),
+      genderId: Joi.number().required(),
+      height: Joi.number().required(),
+      weight: Joi.number().required(),
+      phone: Joi.string().required(),
+    },
+  }),
   ensureDoctorAuthenticated,
   createPatientController.handle
 );
