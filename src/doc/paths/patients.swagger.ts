326cb5444041f899f75ea4e78f0f57bffafa0988
Herick Exterkoetter <herickherick47@gmail.com>
2023-07-02 01:58:08 -0300

    doc: add swagger to update patient route

--- src/doc/paths/patients.swagger.ts
+++ src/doc/paths/patients.swagger.ts
@@ -109,7 +109,7 @@ export const createPatient = {
   },
 };
 
-export const getPatient = {
+export const getAndUpdatePatient = {
   get: {
     description: "Get patient by doctor.",
     tags: ["Patients"],
@@ -158,6 +158,127 @@ export const getPatient = {
               },
               {
                 message: "Patient not found!",
+              },
+            ],
+          },
+        },
+      },
+      "401": {
+        content: {
+          "application/json": {
+            example: [
+              {
+                error: true,
+                code: "token.expired",
+                message: "Token invalid.",
+              },
+              {
+                error: true,
+                code: "token.invalid",
+                message: "Token not present.",
+              },
+            ],
+          },
+        },
+      },
+    },
+  },
+  put: {
+    description:
+      "Update a patient. Obs: GenderId 1 = Feminine, GenderId 2 = Masculine",
+    tags: ["Patients"],
+    parameters: [
+      {
+        schema: {
+          type: "string",
+        },
+        in: "path",
+        name: "patientId",
+        required: true,
+      },
+    ],
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
+    requestBody: {
+      content: {
+        "application/json": {
+          schema: {
+            type: "object",
+            properties: {
+              birthDate: {
+                type: "string",
+              },
+              email: {
+                type: "string",
+              },
+              name: {
+                type: "string",
+              },
+              phone: {
+                type: "string",
+              },
+              genderId: {
+                type: "number",
+              },
+              height: {
+                type: "number",
+              },
+              weight: {
+                type: "number",
+              },
+            },
+          },
+          example: {
+            birthDate: "09/01/2003",
+            email: "patient-example@gmail.com",
+            genderId: 1,
+            height: 170,
+            name: "Patient Example",
+            phone: "48999999999",
+            weight: 68.8,
+          },
+        },
+      },
+    },
+    responses: {
+      "200": {
+        content: {
+          "application/json": {
+            example: {
+              id: "uuid",
+              name: "Patient Example",
+              phone: "48999999999",
+              email: "patient-example@gmail.com",
+              height: 170,
+              weight: 68.8,
+              birth_date: "2003-09-01T00:00:00.000Z",
+              gender_id: 1,
+              doctor_id: "uuid",
+              created_at: "2023-07-02T03:10:05.808Z",
+              updated_at: "2023-07-02T04:52:52.909Z",
+              active: true,
+            },
+          },
+        },
+      },
+      "400": {
+        content: {
+          "application/json": {
+            example: [
+              {
+                message: "Doctor not found!",
+              },
+              {
+                message: "Patient not found!",
+              },
+              {
+                message: "Email address already used.",
+              },
+              {
+                message: "Phone number already used.",
               },
             ],
           },
